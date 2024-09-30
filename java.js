96d4599c4ddb8d287845969763bdf76c1d759a3a
jship90-ux <jship90@wgu.edu>
2024-09-30 11:35:48 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+<script>
+    function checkemail(event) {
+        let }x = document.forms["webform"]["email"].value;
+        if (x!==document.forms["webform"]["email"].value) {
+          alert("Emails don not match!")}
+          return false; 
+        
+    
+</script>
